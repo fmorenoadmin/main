85fc58f5fc09ac3a0d28bcda538687825ea2cccc
Frank Martin Moreno Alburqueque <admin@frankmorenoalburqueque.com>
2018-09-05 16:42:38 -0500

    Update funciones.php

--- funciones.php
+++ funciones.php
@@ -1,25 +1,25 @@
 <?php  
+class Funciones {
+	function conexion($dbname, $usuario, $contrasena){
+		try {
+			$conexion = new PDO("mysql:host=localhost;dbname=$dbname", "$usuario", "$contrasena");
+			return $conexion;
+		} catch (PDOException $e) {
+			return false;
+		}
+	}
 
-function conexion($dbname, $usuario, $contrasena){
-	try {
-		$conexion = new PDO("mysql:host=localhost;dbname=$dbname", "$usuario", "$contrasena");
-		return $conexion;
-	} catch (PDOException $e) {
-		return false;
+	function limpiarDatos($datos){
+		$datos = trim($datos);
+		$datos = stripslashes($datos);
+		$datos = htmlspecialchars($datos);
+		return $datos;
+	}
+
+	function comprobarSession(){
+		if (!isset($_SESSION['admin'])) {
+			header('Location: ' .  RUTA);
+		}
 	}
 }
-
-function limpiarDatos($datos){
-	$datos = trim($datos);
-	$datos = stripslashes($datos);
-	$datos = htmlspecialchars($datos);
-	return $datos;
-}
-
-function comprobarSession(){
-	if (!isset($_SESSION['admin'])) {
-		header('Location: ' .  RUTA);
-	}
-}
-
 ?>
